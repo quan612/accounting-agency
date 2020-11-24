cb5f41fab3f5e325371f69661c689238f3bca9f0
quan612 <quan612@yahoo.com>
2020-11-24 01:29:16 -0500

    test email

---
+++ src/lambda/hello.js
@@ -0,0 +1,15 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+console.log(process.env.MAILGUN_API_KEY)
+
+export function handler(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    // That's the status code which will be returned when the function is executed to indicates that everything is OK
+    body: JSON.stringify({
+      msg: "Hello, World! ",
+    }),
+  })
+}
